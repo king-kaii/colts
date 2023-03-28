e21d9227dd7956b4c44144660facda9926aeca2e
kumar4java <kumar4javatraining@gmail.com>
2023-03-28 08:55:52 +0530

    Final JUnit Commit

--- eureka-app/scorecheck/src/main/java/com/colt/scorecheck/service/ScoreCheckService.java
+++ eureka-app/scorecheck/src/main/java/com/colt/scorecheck/service/ScoreCheckService.java
@@ -5,8 +5,7 @@ import java.util.Random;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ScoreCheckService {
-
+public class ScoreCheckService {	 
 	public int checkScore(int borrowerId) {
 		Random randomObj = new Random();
 		int randomNum = randomObj.nextInt((800 - 700)) + 700;
